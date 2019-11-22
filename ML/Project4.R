6b67f050c3f4cc7043b4ca6376990ca08544e7e2
Alexander Bech <alexanderlillevangbech@gmail.com>
2019-11-22 10:59:11 +0100

    add

--- ML/Project4.R
+++ ML/Project4.R
@@ -1,131 +1,47 @@
-
-## Input data paths here
-
 path <- paste(getwd(),"/data/",sep = "")
-path_train <- paste(path,"IrisTrainML.dt.txt",sep="")
-path_test <- paste(path,"IrisTestML.dt.txt",sep="")
+path_projmatrix <- paste(path,"projection_matrix.csv",sep="")
+path_MLtrain <- paste(path,"MLWeedCropTrain.csv",sep="")
+
+data_projmatrix <- read.csv(path_projmatrix,header = FALSE)
+data_MLtrain <- read.csv(path_MLtrain, header = FALSE)
+
+train_x <- matrix(unlist(data_MLtrain[,1:13]),ncol = 13, nrow = nrow(data_MLtrain))
+train_y <- unlist(data_MLtrain[,14])
+projmatrix <- matrix(unlist(data_projmatrix),ncol = ncol(data_projmatrix), nrow = nrow(data_projmatrix))
+
+train_trans_x <- t((projmatrix)%*%t(train_x))
+
+dat <- data.frame(train_trans_x,train_y)
+rbPal <- colorRampPalette(c("red","green"))
+dat$Col <- rbPal(2)[as.numeric(cut(dat$train_y,breaks = 2))]
+plot(dat$X1,dat$X2, pch = 20,col = dat$Col, xlab = "X1", ylab = "X2")
+legend("right",title="Legend",legend=c("Weed","Crop"),col =rbPal(2),pch=20)
+title("Scatterplot of Weeds/Crops distribution")
 
 
-SteepGradiant <- function(y,x,eta,TimeSteps = 1000, w = rnorm(ncol(x),mean = 0,sd = 0.01)) 
-{
-  if(!is.vector(y) && !is.matrix(x) && length(y) != nrow(x)) {return (NA)}
-  ColDim <- ncol(x); RowDim <- nrow(x)
+#Problem 2
 
-  t <-  1
-  constant <- -(1/RowDim)
-  while (t < TimeSteps)
-  {
-    gradiant <- rep(0,ColDim)
-    for (i in 1:RowDim)
-    {
-    gradiant <- gradiant + constant*(y[i]*x[i,])/(1+exp(y[i]*(c(t(w)%*%x[i,]))))
-    }
-    w <- w - eta*gradiant
-    t <- t + 1
-  }
-  return (w)
-}
+Center_data <- train_trans_x[1:2,1:2]
+kmeans_2 <- kmeans(train_trans_x, centers = Center_data)
 
+plot(dat$X1,dat$X2, pch = 20,col = dat$Col, xlab = "X1", ylab = "X2")
+legend("right",title="Legend",legend=c("Weed","Crop","Clusters"),col =c(rbPal(2),"black"),pch=20)
+title("Scatterplot of Weeds/Crops with clutsering")
+points(kmeans_2$centers[1], kmeans_2$centers[3],cex = 1.8, pch = 16, col = "black")
+points(kmeans_2$centers[2], kmeans_2$centers[4], cex = 1.8, pch = 16, col = "black")
 
-
-loss_fun <- function(y_predict,y_label) ## 0-1 loss function
-{
-  if (length(y_predict) != length(y_label)) {return (NA)}
-  Return_Vector <- rep(NA,length(y_predict))
-  
-  for (i in 1:length(y_predict))
-  {
-    if (y_predict[i] == y_label[i])
-    {
-      Return_Vector[i] <- 0
-    }
-    else
-    {
-      Return_Vector[i] <- 1
-    }
-  }
-  
-  return (Return_Vector)
-}
-
-theta_fun <- function(x) {return (1/(1+exp(-x)))}
-avg <- function(x) {return (sum(x)/length(x))}
-
-
-
-
-## yDgata handling start
-
-
-DataTrain <- read.table(path_train,header = FALSE)
-DataTest <- read.table(path_test,header = FALSE)
-
-DataTrain <- DataTrain[!(DataTrain$V3 == 2),] ## Remove rows with 2's
-DataTrain[DataTrain$V3 == 0,3] <- -1 ## replace 0 with -1
-
-DataTest <- DataTest[!(DataTest$V3 == 2),] ## Remove rows with 2's
-DataTest[DataTest$V3 == 0,3] <- -1 ## replace 0 with -1
-
-train_y <- DataTrain[,3] ## insert in vector form
-train_x <- matrix(1, nrow = nrow(DataTrain), ncol = 3, byrow = TRUE) ## insert to matrix form
-train_x[,1] <- DataTrain[,1]
-train_x[,2] <- DataTrain[,2]
-
-test_y <- DataTest[,3] ## insert in vector form
-test_x <- matrix(1, nrow = nrow(DataTest), ncol = 3, byrow = TRUE) ## insert to matrix form
-test_x[,1] <- DataTest[,1]
-test_x[,2] <- DataTest[,2]
-
-## Data handling stop
-## We have test_x, train_x matrix's
-## We have test_y, train_y vector's
-
-
-LearningRate <- 0.001
-w <- SteepGradiant(train_y,train_x,LearningRate, TimeSteps = 10000)
-
-
-### Try on data
-predict_y_train <- rep(NA,nrow(train_x))
-for (i in 1:nrow(train_x))
-{
-temp <- t(w[1:2])%*%train_x[i,1:2] + w[3]
-if (sign(temp) == (-1))
-{
-predict_y_train[i] <- -1
-} else
-{
-  predict_y_train[i] <- 1
-}
-}
-results_train <- loss_fun(predict_y_train,train_y)
-plot(results_train, xlab = "Training data index", ylab = "0/1 Loss Frequency",sub = "Figure 4")
-abline(h = avg(results_train))
-text(20,y = (avg(results_train)+0.02), paste("Average Frequency of guessing wrong:",format(avg(results_train),digits = 2)))
-title("Train Data")
-
-## test
-predict_y_test <- rep(NA,nrow(test_x))
-for (i in 1:nrow(test_x))
-{
-  temp <- t(w[1:2])%*%test_x[i,1:2] + w[3]
-  if (sign(temp) == (-1))
-  {
-    predict_y_test[i] <- -1
-  } else
-  {
-    predict_y_test[i] <- 1
+newCluster <- kmeans_2$cluster
+for (i in 1:length(kmeans_2$cluster)) {
+  if (kmeans_2$cluster[i] == 2) {
+  newCluster[i] <- 0    
   }
 }
-results_test <- loss_fun(predict_y_test,test_y)
-plot(results_test, xlab = "Testing data index", ylab = "0/1 Loss Frequency",sub = "Figure 4")
-abline(h = avg(results_test))
-text(10,y = (avg(results_test)+0.02), paste("Average Frequency of guessing wrong:",format(avg(results_test),digits = 2)))
-title("Test Data")
+count <- 0
+for (i in 1:length(newCluster)) {
+  if (newCluster[i] == train_y[i]) {
+    count <- count + 1
+  }
+}
+#hit ratio 
+count/length(kmeans_2$cluster)
 
-
-
-
-w <- matrix(w,nrow = 1, ncol =3)
-colnames(w) <- c("w_1","w_2","b")
-w
---
+++ ML/README.md
@@ -0,0 +1,23 @@
+## **Machine Learning** #
+This folder contains a small collection of ML algorithms I've implemented. Any work on neural networks has been in tensorflow and is located in my SABR_representation folder.
+
+#### C++ Programs
+I've implemented K-Nearest Neighbour and Logistic Regression in C++. The primary reason for doing this was to test out the Armadillon linear algebra library, and make sure that I was able to seamlessly read online documentation of the library and use it effectivly.
+
+
+#### R programs
+For the R projects, using Rstudio is recommended to view graphs. All data needed to run the R programs is located in the data folder. 
+
+1. **Project1 (K-Nearest Neighbour)**
+
+    K-Nearest Neighbour on the MINST dataset. Big dataset, so depending on the computer, it might take between 10-30 min to run.
+
+2. **Project2 (Linear Regression)**
+
+    Basic Linear Regression implentation used on DanWood dataset.
+    
+3. **Project3 (Logistic Regression)**
+
+      Implentation of Logistic Regression on the IRIS dataset.
+
+4. **Project4 (Principal component analysis)**
