50858d6483cc9297c5d98ed26cb625bbb3750dd5
Alexander Bech <alexanderlillevangbech@gmail.com>
2020-02-01 16:26:13 +0100

    env

--- leetcode/lc.cpp
+++ leetcode/lc.cpp
@@ -1,2 +1,2 @@
 /** Author: Alexander Bech* Leetcode Problem Solution* Leetcode ID: 
-*/#include <iostream>class Solution {public:};/* Testing */int main(){	std::cout << "hello world" << std::endl;	return 0;}
+*/#include <iostream>class Solution {public:};/* Testing */int main(){	std::cout << "hello world" << std::endl;}
--- leetcode/template/lc.bat
+++
@@ -1,6 +0,0 @@
-set "arg=%1%"
-set "ph=C:\Users\alexa\Desktop\alillevangbech\leetcode\template\template"
-set "g="C:\Program Files\Notepad++\notepad++.exe""
-set "f=C:\Users\alexa\Desktop\alillevangbech\leetcode\#%arg%lc.cpp"
-%ph% %arg%
-%g% %f%
