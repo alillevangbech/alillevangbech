073eb2598c621d6e0e7076c4d915560880407641
Alexander Bech <alexanderlillevangbech@gmail.com>
2020-02-07 14:30:16 +0100

    update

--- leetcode/template/conda.bat
+++ leetcode/template/conda.bat
@@ -2,4 +2,4 @@
 cd /..
 call C:\Users\alexa\Anaconda3\Scripts\activate.bat C:\Users\alexa\Anaconda3
 cd C:\Users\alexa\Desktop\alillevangbech\leetcode
-call python fMakefile.py 66lc
+call python fMakefile.py 88lc
